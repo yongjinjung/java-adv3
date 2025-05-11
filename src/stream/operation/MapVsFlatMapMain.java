a4e9d450e6a90d5277f53e235539bddacb1168c0
jungyongjin <jyj1641@gmail.com>
2025-05-11 16:56:50 +0900

    8. 스트림 API2 - 기능

---
+++ src/stream/operation/MapVsFlatMapMain.java
@@ -0,0 +1,40 @@
+package stream.operation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class MapVsFlatMapMain {
+
+    public static void main(String[] args) {
+        List<List<Integer>> outerList = List.of(
+                List.of(1,2),
+                List.of(3,4),
+                List.of(5,6)
+        );
+        System.out.println("outerList = " + outerList);
+
+        //for
+        List<Integer> forResult = new ArrayList<>();
+        for(List<Integer> list : outerList) {
+            for(Integer i : list) {
+                forResult.add(i);
+            }
+        }
+        System.out.println("forResult = " + forResult);
+
+        //map
+        List<Stream<Integer>> streamList = outerList.stream()
+                .map(Collection::stream)
+                .toList();
+        System.out.println("streamList = " + streamList);
+
+
+        //flatMap
+        List<Integer> flatMapResult = outerList.stream()
+                .flatMap(list -> list.stream())
+                .toList();
+        System.out.println("flatMapResult = " + flatMapResult);
+    }
+}
